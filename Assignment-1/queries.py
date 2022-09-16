a6808d6885c043bc8620a39f54226dbb3c36489d
Anuoluwapo Faboro <anufab04@gmail.com>
2022-09-16 02:20:26 -0400

    completed queries 0 - 15, 6 and 14 need more work

--- Assignment-1/queries.py
+++ Assignment-1/queries.py
@@ -15,9 +15,12 @@ order by Id asc;
 ### Output column order: Id, Title, Tags
 ### Order by Id ascending
 queries[1] = """
-select 0;
+select Id, Title, Tags
+from posts
+where length(Tags) > 0
+and Tags like '%<postgresql-9.4>%'
+order by Id asc;
 """
-
 
 ### 2. Write a query to output the number of years users have been on the
 ### platform (assuming they started on 'CreationDate') as of September 1, 2022
@@ -26,7 +29,10 @@ select 0;
 ### Output columns: Id, DisplayName, Num_years
 ### Order output by Num_years increasing, and then by Id ascending
 queries[2] = """
-select 0;
+select Id, DisplayName, date_part('year', age('2022-09-01', CreationDate)) as Num_years
+from users
+where DisplayName = 'Jason'
+order by Num_years asc, Id asc;
 """
 
 ### 3. Select all the "distinct" years that users with names starting with 'M'
@@ -34,7 +40,10 @@ select 0;
 ### Output column: Year
 ### Order output by Year ascending
 queries[3] = """
-select 0;
+select distinct date_part('year', CreationDate) as Year
+from users
+where DisplayName like 'M%'
+order by Year asc;
 """
 
 ### 4. Write a query to find users who have, on average, given at least 1 UpVote per
@@ -45,14 +54,22 @@ select 0;
 ### Output columns: Id, DisplayName, CreationDate, UpVotes
 ### Order by Id ascending
 queries[4] = """
-select 0;
+select Id, DisplayName, CreationDate, Upvotes
+from users
+where ('2022-09-01' - CreationDate) > 0 
+and (Upvotes/('2022-09-01' - CreationDate)) >= 1
+order by Id asc;
 """
 
 ### 5. Write a single query to report all Badges for the users with reputation between 10000 and 11000, by joining Users and Badges.
 ### Output Column: Id (from Users), DisplayName, Name (from Badges), Reputation
 ### Order by: Id increasing
 queries[5] = """
-select 0;
+select users.Id, DisplayName, Name, Reputation
+from users left outer join badges on (users.Id = badges.userId)
+where Reputation >= 10000 
+and Reputation <= 11000
+order by users.Id asc;
 """
 
 ### 6. Write a query to find all Posts who satisfy one of the following conditions:
@@ -62,14 +79,19 @@ select 0;
 ### Output columns: Id, Title, ViewCount
 ### Order by: Id ascending
 queries[6] = """
-select 0;
+select Id, Title, ViewCount
+from posts
+where (lower(Title) like '%postgres%' and ViewCount >= 50000)
+or (lower(Title) like '%mongodb%' and ViewCount <= 25000)
+order by Id asc;
 """
-
 
 ### 7. Count the number of the Comments made by the user with DisplayName 'JHFB'.
 ### Output columns: Num_Comments
 queries[7] = """
-select 0;
+select count(*) as Num_Comments
+from comments left outer join users on (users.Id = comments.userId)
+where users.DisplayName = 'JHFB';
 """
 
 ### 8. Find the Users who have received badges with names: "Guru" and "Curious". 
@@ -78,7 +100,13 @@ select 0;
 ### Output columns: UserId
 ### Order by: UserId ascending
 queries[8] = """
-select 0;
+select distinct UserId
+from badges
+where Name = 'Curious'
+intersect
+select distinct UserId
+from badges
+where Name = 'Guru';
 """
 
 ### 9. "Tags" field in Posts lists out the tags associated with the post in the format "<tag1><tag2>..<tagn>".
@@ -87,9 +115,12 @@ select 0;
 ### Output columns: Id, Title, Tags
 ### Order by: Id ascending
 queries[9] = """
-select 0;
+select Id, Title, Tags
+from posts 
+where Tags like '%<postgresql>%'
+and cardinality(string_to_array(Tags, '><')) >= 6
+order by Id asc;
 """
-
 
 ### 10. SQL "with" clause can be used to simplify queries. It essentially allows
 ### specifying temporary tables to be used during the rest of the query. See Section
@@ -106,7 +137,11 @@ with temp as (
         from users, comments 
         where users.id = comments.userid 
         group by users.id, users.displayname)
-select 0;
+select Id, DisplayName, num_Comments
+from temp
+where num_Comments = (select max(num_Comments) from temp)
+group by Id, DisplayName, num_Comments
+order by Id asc;
 """
 
 ### 11. List the users who posted no comments and with at least 500 views. 
@@ -114,9 +149,12 @@ select 0;
 ### Output Columns: Id, DisplayName
 ### Order by Id ascending
 queries[11] = """
-select 0;
+select users.Id, DisplayName
+from users
+where users.Id not in (select userId from comments)
+and views >= 500
+order by users.Id asc;
 """
-
 
 ### 12. Write a query to output a list of posts with comments, such that PostType = 'Moderator nomination' 
 ### and the comment has score of at least 10. So there may be multiple rows with the same post
@@ -124,9 +162,12 @@ select 0;
 ### Output: Id (Posts), Title, Text (Comments)
 ### Order by: Id ascending
 queries[12] = """
-select 0;
+select posts.Id, Title, comments.Text
+from posts left outer join comments on (posts.Id = comments.PostId)
+where posts.PostTypeId = 6
+and comments.score >= 10
+order by posts.Id asc;
 """
-
 
 ### 13. Generate a list - (Badge_Name, Num_Users) - containing the different
 ### badges, and the number of users who received those badges.
@@ -135,7 +176,11 @@ select 0;
 ### Order by Badge_name asc
 ### Use LIMIT to limit the output to first 20 rows.
 queries[13] = """
-select 0;
+select Name, count(distinct UserId) as Num_Users
+from badges
+group by Name
+order by Name asc
+limit 20;
 """
 
 ### 14. For each post, count the number of comments for that post.
@@ -153,7 +198,10 @@ select 0;
 ### Output Columns: Id, Num_Comments
 ### Order by: Id ascending
 queries[14] = """
-select 0;
+select posts.Id, count(*) as Num_Comments
+from posts left outer join comments on (posts.Id = comments.PostId)
+group by posts.Id
+order by posts.Id asc;
 """
 
 
@@ -170,7 +218,12 @@ select 0;
 ### Output columns: Reputation, Num_users
 ### Order by Reputation ascending
 queries[15] = """
-select 0;
+select gen.rep as Reputation, count(users.id)
+from (select * from generate_series(1, 100) as rep) gen
+left outer join users
+on (gen.rep = users.reputation)
+group by gen.rep
+order by gen.rep asc;
 """
 
 
