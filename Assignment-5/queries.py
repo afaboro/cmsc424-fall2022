2fc1d3a5eed3befe8ecc5ff03c26a71679767004
Anuoluwapo Faboro <anufab04@gmail.com>
2022-11-30 16:00:05 -0500

    completed assignment 5

--- Assignment-5/queries.py
+++ Assignment-5/queries.py
@@ -1,47 +1,76 @@
+import bson
+
 ### 0. Return all the entries in accounts collection
 def query0(db):
     return db['accounts'].find({})
 
 ### 1. Find all information for the customer with username 'fmiller'
 def query1(db):
-    return []
+    return db['customers'].find({"username": 'fmiller'})
 
 ### 2. For all customers with first name 'Natalie', return their username, name, and address
 ### Use 'regex' functionality to do the matching
 def query2(db):
-    return []
+    return db['customers'].find(
+        {"name": {"$regex": "Natalie"}}, 
+        {"username": 1, "name": 1, "address": 1})
 
 ### 3. Find all accounts with a 'products' array containing 'Commodity' -- return the '_id' and 'account_id'
 def query3(db):
-    return []
+    return db['accounts'].find({"products": "Commodity"}, {"account_id": 1})
 
 ### 4. Find all accounts with either limit <= 9000 or products array exactly ["Commodity", "InvestmentStock"] in that order -- return the entire accounts information
 ### Use "$or" to do a disjunction
 def query4(db):
-    return []
+    return db['accounts'].find({"$or": 
+        [
+            {"limit": {"$lte": 9000}}, 
+            {"products": ["Commodity", "InvestmentStock"]}
+        ]
+    })
 
 ### 5. Find all accounts with limit <= 9000 AND products array exactly ["Commodity", "InvestmentStock"] in that order -- return the entire accounts information
 def query5(db):
-    return []
-
+    return db['accounts'].find({"$and": 
+        [
+            {"limit": {"$lte": 9000}},
+            {"products": ["Commodity", "InvestmentStock"]}
+        ]
+    })
 
 ### 6. Find all accounts where the second entry in the products array is 'Brokerage' -- return the entire accounts information
 def query6(db):
-    return []
+    return db['accounts'].find({"products.1": "Brokerage"})
 
 ### 7. On the customers collection, use aggregation and grouping to find the number of customers born in each month
 ### The output will contain documents like: {'_id': 7, 'totalCount': 42} 
 ### Use '$month' function to operate on the dates, and use '$sum' aggregate to do the counting
 ### https://database.guide/mongodb-month/
 def query7(db):
-    return []
+    pipeline = [
+        {"$project": {"birthMonth": {"$month": "$birthdate"}}},
+        {"$group": 
+            {"_id": "$birthMonth", "totalCount": {"$sum": 1}}
+        }
+    ]
+    return db['customers'].aggregate(pipeline)
 
 ### 8. Modify the above query to only count the customers whose name starts with any letter between 'A' and 'G' (inclusive). 
 ### The output will contain documents like: {'_id': 2, 'totalCount': 17}
 ###
 ### Use '$match' along with '$group' as above.
 def query8(db):
-    return []
+    pipeline = [
+        {"$project": {
+            "birthdate": 1,
+            "firstLetter": {"$substrCP": ["$name", 0, 1]}}},
+        {"$match": {"firstLetter": {"$regex": "^[A-G]"}}},
+        {"$project": {
+            "firstLetter": 1,
+            "birthMonth": {"$month": "$birthdate"}}},
+        {"$group": {"_id": "$birthMonth", "totalCount": {"$sum": 1}}}
+    ]
+    return db['customers'].aggregate(pipeline)
 
 ### 9. In the 'transactions' collection, all transactions are inside a single array, making it difficult to operate on them. 
 ### However, we can use 'unwind' to create a separate document for each of the transactions. 
@@ -55,7 +84,17 @@ def query8(db):
 ### {'_id': ObjectId('5ca4bbc1a2dd94ee58161cd5'), 'account_id': 463155, 'transactions': {'amount': 6691, 'symbol': 'amd', 'transaction_code': 'buy'}}
 ###
 def query9(db):
-    return []
+    pipeline = [
+        {"$match": {"transaction_count": {"$lt": 10}}},
+        {"$unwind": "$transactions"},
+        {"$project": {
+            "_id": 1,
+            "account_id": 1,
+            "transactions.amount": 1,
+            "transactions.symbol": 1,
+            "transactions.transaction_code": 1}}
+    ]
+    return db['transactions'].aggregate(pipeline)
 
 ### 10. Use the result of the above query to compute the total number shares sold or bought for each symbol across the entire collection of accounts
 ### However, DO NOT restrict it to only accounts with < 10 transactions.
@@ -67,7 +106,19 @@ def query9(db):
 ###       {'_id': 'goog', 'totalCount': 27029894}
 ###       {'_id': 'nvda', 'totalCount': 26108705}
 def query10(db):
-    return []
+    pipeline = [
+        {"$unwind": "$transactions"},
+        {"$project": {
+            "_id": 0,
+            "amount": "$transactions.amount",
+            "symbol": "$transactions.symbol"}},
+        {"$group": {
+            "_id": "$symbol",
+            "totalCount": {"$sum": "$amount"}
+        }},
+        {"$sort": {"totalCount": -1}}        
+    ]
+    return db['transactions'].aggregate(pipeline)
 
 ### 11. Use $lookup to do a "join" between customers and accounts to find, for each customer the number of accounts they have with 'InvestmentFund' as a product (i.e., the number of their accounts where 'InvestmentFund' is in the 'products' array).
 ### Sort the final output by 'username' in the ascending order
@@ -78,7 +129,33 @@ def query10(db):
 ###        {'_id': 'allenhubbard', 'totalCount': 2}
 ###        {'_id': 'alvarezdavid', 'totalCount': 3}
 def query11(db):
-    return []
+    pipeline = [
+        {"$unwind": "$accounts"},
+        {"$project": {
+            "_id": 0,
+            "username": 1,
+            "account": "$accounts"
+        }},
+        {"$lookup": {
+            "from": "accounts",
+            "localField": "account",
+            "foreignField": "account_id",
+            "as": "account_info"
+        }},
+        {"$unwind": "$account_info"},
+        {"$project": {
+            "account": 1,
+            "username": 1,
+            "products": "$account_info.products"
+        }},
+        {"$match": {"products": "InvestmentFund"}},
+        {"$group": {
+            "_id": "$username",
+            "totalCount": {"$sum": 1}
+        }},
+        {"$sort": {"_id": 1}}
+    ]
+    return db['customers'].aggregate(pipeline)
 
 ### 12. We want to find all accounts that have exactly 3 products and <= 10 transactions associated with them.
 ### Use '$lookup' and '$group' to do so by joining accounts and transactions. This would be a multi-stage pipeline, possibly with multiple groups and matches.
@@ -92,7 +169,34 @@ def query11(db):
 ### Use unwind and addFields to add the top-level field 'transaction_count' to accounts
 ### Sort the final output by account_id
 def query12(db):
-    return []
+    pipeline = [
+        {"$project": {
+            "_id": 1,
+            "account_id": 1,
+            "products": 1
+        }},
+        {"$lookup": {
+            "from": "transactions",
+            "localField": "account_id",
+            "foreignField": "account_id",
+            "as": "transaction_info"
+        }},
+        {"$project": {
+            "_id": 1,
+            "account_id": 1,
+            "products": 1,
+            "transaction_count": "$transaction_info.transaction_count"
+        }},
+        {"$unwind": "$transaction_count"},
+        {"$match": {"$and":
+            [
+                {"transaction_count": {"$lte": 10}},
+                {"products": {"$size": 3}}
+            ]
+        }},
+        {"$sort": {"account_id": 1}}
+    ]
+    return db['accounts'].aggregate(pipeline)
 
 ### 13. Using an aggregation and '$addToSet' and '$first', write a query whose output has a multi-attribute _id comprising of account_id and year, with two aggregates: 
 ### a set listing all the symbols that were traded by that account in that year, and 
@@ -103,8 +207,36 @@ def query12(db):
 ### {'_id': {'account_id': 50948, 'year': 2004}, 'traded_symbols': ['csco'], 'first_symbol': 'csco'},
 ### {'_id': {'account_id': 50948, 'year': 2006}, 'traded_symbols': ['aapl'], 'first_symbol': 'aapl'},
 def query13(db):
-    return []
-
+    pipeline = [
+        {"$project": {
+            "_id": 0,
+            "account_id": 1,
+            "transactions": 1
+        }},
+        {"$unwind": "$transactions"},
+        {"$project": {
+            "account_id": 1,
+            "transaction_year": {"$year": "$transactions.date"},
+            "transaction_symbol": "$transactions.symbol"
+        }},
+        {"$group": {
+            "_id": {"account_id": "$account_id", "year": "$transaction_year"},
+            "traded_symbols": {"$addToSet": "$transaction_symbol"}
+        }},
+        {"$project": {
+            "_id": 1,
+            "traded_symbols": {
+                "$sortArray": {"input": "$traded_symbols", "sortBy": 1}
+            }
+        }},
+        {"$project": {
+            "_id": 1,
+            "traded_symbols": 1,
+            "first_symbol": {"$first": "$traded_symbols"}
+        }},
+        {"$sort": {"_id": 1}}        
+    ]
+    return db['transactions'].aggregate(pipeline)
 
 ### 14. Create an output that associates each "product" (found in 'accounts') and each "symbol" (found in 'transactions') with the total volume
 ### of shares bought/sold (i.e., 'amount' in each transaction).
@@ -116,7 +248,36 @@ def query13(db):
 ### {'_id': {'product': 'Brokerage', 'symbol': 'amzn'}, 'total_amount': 9802},
 ### {'_id': {'product': 'Brokerage', 'symbol': 'bb'}, 'total_amount': 1415},
 def query14(db):
-        return []
+    pipeline = [
+        {"$match": {"transaction_count": {"$eq": 3}}},
+        {"$unwind": "$transactions"},
+        {"$project": {
+            "_id": 0,
+            "account_id": 1,
+            "amount": "$transactions.amount",
+            "symbol": "$transactions.symbol",
+        }},
+        {"$lookup": {
+            "from": "accounts",
+            "localField": "account_id",
+            "foreignField": "account_id",
+            "as": "acct_info"
+        }},
+        {"$project": {
+            "account_id": 1,
+            "amount": 1,
+            "symbol": 1,
+            "products": "$acct_info.products",
+        }},
+        {"$unwind": "$products"},
+        {"$unwind": "$products"},
+        {"$group": {
+            "_id": {"product": "$products", "symbol": "$symbol"},
+            "total_amount": {"$sum": "$amount"}
+        }},
+        {"$sort": {"_id": 1}}
+    ]
+    return db['transactions'].aggregate(pipeline)
 
 ### 15. Let's create a copy of the accounts collection using: db['accounts'].aggregates([ {"$out": "accounts_copy"} ])
 ### Write the code to insert 10 more documents into the collection with data:
@@ -129,7 +290,66 @@ def query15(db):
     db['accounts'].aggregate([ {"$out": "accounts_copy"} ])
 
     db['accounts_copy'].insert_many([
-### COMPLETE THE COMMAND
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a61"),
+            "account_id": 11,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a62"),
+            "account_id": 12,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a63"),
+            "account_id": 13,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a64"),
+            "account_id": 14,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a65"),
+            "account_id": 15,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a66"),
+            "account_id": 16,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a67"),
+            "account_id": 17,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a68"),
+            "account_id": 18,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a69"),
+            "account_id": 19,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        },
+        {
+            "_id": bson.ObjectId("5ca4bbc7a2dd94ee58162a6a"),
+            "account_id": 20,
+            "limit": 10000,
+            "products": ["Brokerage", "InvestmentStock"]
+        }
     ])
 
     return db['accounts_copy'].find( {"account_id": {"$gte": 11, "$lte": 20}} )
@@ -147,7 +367,12 @@ def query15(db):
 def query16(db):
     db['customers'].aggregate([ {"$out": "customers_copy"} ])
 
-### USE update_many to do the required update
+    db['customers_copy'].update_many(
+        {}, 
+        [
+            {"$set":{"summary": {"num_accounts": {"$size": "$accounts"}}}}
+        ]
+    )
 
     return db['customers_copy'].find({}, {"summary": 1})
 
@@ -175,8 +400,26 @@ def query17(db):
     db['customers'].aggregate([ {"$out": "customers_copy2"} ])
 
     db['customers_copy2'].aggregate([
-### COMPLETE THE COMMAND
-            {'$merge': { 'into': "customers_copy2", 'on': "_id", 'whenMatched': "merge", 'whenNotMatched': "insert" }}
+        {'$unwind': "$accounts"},
+        {'$lookup': {
+            "from": "accounts",
+            "localField": "accounts",
+            "foreignField": "account_id",
+            "as": "account_info"
+        }},
+        {'$project': {
+            "accounts": 1,
+            "allProducts": "$account_info.products"
+        }},
+        {'$unwind': "$allProducts"},
+        {'$unwind': "$allProducts"},
+        {'$group': {
+            "_id": "$_id",
+            "accounts": {"$addToSet": "$accounts"},
+            "allProducts": {"$addToSet": "$allProducts"}
+        }},
+        {'$merge': { 'into': "customers_copy2", 'on': "_id", 'whenMatched': "merge", 'whenNotMatched': "insert" }}
     ])
 
+    #return db['customers_copy2'].find({})
     return db['customers_copy2'].find({}, {'allProducts': 1})
